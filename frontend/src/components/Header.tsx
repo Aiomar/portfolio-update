88b1237ca8b6a6b5b7d2f27b959b4fcd8b3f3c24
Omar Aidi <aidi360omar@gmail.com>
2025-10-09 17:26:11 +0100

    updated nav and its integration in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,27 +1,31 @@
 import { Moon, Sun, XCircle } from "lucide-react";
+import Nav from "./Nav";
 
-export default function Header({theme, updateTheme, toggleNavBar, isPhoneNavOpen, currentVisibleSection}:props) {
-  
+export default function Header(
+  {theme, updateTheme, toggleNavBar, isPhoneNavOpen, currentVisibleSection}:props
+){
   return (
     <header
       className="flex md:flex-row md:justify-center fixed right-0 left-0 z-50 
-      max-h-96 w-full bg-slate-100 dark:bg-gray-900  "
+      max-h-96 w-full bg-slate-100 dark:bg-gray-900"
     >
       <nav
-        className="flex md:justify-center md:ml-0 w-full px-4 min-h-24 bg-slate-100 dark:bg-gray-900  
-        lg:px-6 py-2.5"
+        className="flex md:justify-center md:ml-0 w-full px-4 min-h-24
+        bg-slate-100 dark:bg-gray-900 lg:px-6 py-2.5"
       >
-        <div className="flex items-center lg:order-2">
+        <div 
+          className="flex items-center lg:order-2"
+        >
           <button
             onClick={toggleNavBar}
             data-collapse-toggle="mobile-menu-2"
             type="button"
-            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden"
+            className="inline-flex items-center p-2 ml-1 text-sm 
+            text-gray-500 rounded-lg lg:hidden"
             aria-controls="mobile-menu-2"
             aria-expanded="false"
           >
             <span className="sr-only">Open main menu</span>
-
             {isPhoneNavOpen ? (
               <XCircle size={30} />
             ) : (
@@ -33,8 +37,9 @@ export default function Header({theme, updateTheme, toggleNavBar, isPhoneNavOpen
               >
                 <path
                   fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 
-                15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1
+                  0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1
+                  1 0 110 2H4a1 1 0 01-1-1z"
                   clipRule="evenodd"
                 ></path>
               </svg>
@@ -42,10 +47,11 @@ export default function Header({theme, updateTheme, toggleNavBar, isPhoneNavOpen
           </button>
         </div>
         <div
-          className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+          className="hidden justify-between items-center w-full 
+          lg:flex lg:w-auto lg:order-1"
           id="mobile-menu-2"
         >
-          <Nav visible={currentVisibleSection} toggleNavBar={toggleNavBar} />
+          <Nav currentVisibleSection={currentVisibleSection} />
         </div>
       </nav>
       <div className="flex flex-col justify-center items-center mr-5">
---
+++ frontend/src/components/Nav.tsx
@@ -0,0 +1,103 @@
+import { Github } from "lucide-react";
+
+export default function Nav({ currentVisibleSection }:props) {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div>
+        <img
+          src="assets/logo.svg"
+          alt=""
+          className="w-10 mr-10 ml-10 md:ml-0 hidden md:flex"
+        />
+      </div>
+      <ul className="flex flex-col  mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-2">
+        <li className="ml-5 md:ml-0">
+          <a
+            href="#about"
+            className={
+              currentVisibleSection === "about"
+                ? "block py-2 pr-4 pl-3 text-sky-500 lg:hover:bg-transparent lg:border-0 lg:p-0"
+                : "block py-2 pr-4 pl-3  hover:text-sky-500 dark:text-white  dark:hover:text-sky-500 lg:hover:bg-transparent lg:border-0 lg:p-0"
+            }
+            aria-current="page"
+            title="about me and social accounts"
+          >
+            <div className="flex md:justify-center items-center">
+              <p className="ml-1 poppins-semibold text-3xl md:text-base">
+                About
+              </p>
+            </div>
+          </a>
+        </li>
+        <li className="ml-5 md:ml-0">
+          <a
+            href="#projects"
+            className={
+              currentVisibleSection === "projects"
+                ? "block py-2 pr-4 pl-3 text-sky-500  lg:hover:bg-transparent lg:border-0 lg:p-0"
+                : "block py-2 pr-4 pl-3  hover:text-sky-500 dark:text-white  dark:hover:text-sky-500 lg:hover:bg-transparent lg:border-0 lg:p-0"
+            }
+            title="checkout some of my work"
+          >
+            <div className="flex md:justify-center items-center">
+              <p className=" ml-1 poppins-semibold text-3xl md:text-base">
+                Projects
+              </p>
+            </div>
+          </a>
+        </li>
+        <li className="ml-5 md:ml-0">
+          <a
+            href="#resume"
+            className={
+              currentVisibleSection === "resume"
+                ? "block py-2 pr-4 pl-3 text-sky-500 lg:hover:bg-transparent lg:border-0 lg:p-0"
+                : "block py-2 pr-4 pl-3  hover:text-sky-500 dark:text-white  dark:hover:text-sky-500 lg:hover:bg-transparent lg:border-0 lg:p-0"
+            }
+            title="more about my carear"
+          >
+            <div className="flex md:justify-center items-center">
+              <p className="ml-1 poppins-semibold text-3xl md:text-base">
+                Resume
+              </p>
+            </div>
+          </a>
+        </li>
+        <li className="ml-5 md:ml-0">
+          <a
+            href="#contact"
+            className={
+              currentVisibleSection === "contact"
+                ? "block py-2 pr-4 pl-3 text-sky-500 lg:hover:bg-transparent lg:border-0 lg:p-0"
+                : "block py-2 pr-4 pl-3  hover:text-sky-500 dark:text-white  dark:hover:text-sky-500 lg:hover:bg-transparent lg:border-0 lg:p-0"
+            }
+          >
+            <div className="flex md:justify-center items-center">
+              <p className="ml-1 poppins-semibold text-3xl md:text-base">
+                Contact
+              </p>
+            </div>
+          </a>
+        </li>
+        <li className="ml-8 md:ml-0">
+          <a
+            href="https://github.com/Aiomar/portfolio"
+            target="_blank"
+            className="scale-110 md:scale-100"
+          >
+            <div
+              className="flex md:justify-center items-center "
+              title="check portfolio on github"
+            >
+              <Github size={25} className="text-black dark:text-white" />
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+type props = {
+  currentVisibleSection: string;
+}
