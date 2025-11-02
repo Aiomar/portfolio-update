f62a9ca244e708e482558e81ac533fb73675b117
Omar Aidi <aidi360omar@gmail.com>
2025-11-02 21:44:25 +0100

    edit bg color in aside

--- client/src/components/Aside.tsx
+++ client/src/components/Aside.tsx
@@ -8,7 +8,7 @@ type props = {
 export default function Aside(props: props) {
   return (
     <aside
-      className="fixed z-30 h-screen w-full bg-white shadow-lg shadow-gray-600 dark:bg-neutral-950"
+      className="fixed z-30 h-screen w-full bg-white shadow-lg shadow-gray-600 dark:bg-neutral-900"
       onClick={props.toggleNavBar}
     >
       <div className="mt-32 lg:hidden">
